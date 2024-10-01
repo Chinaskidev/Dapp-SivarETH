c9dab194bdc8607420f99ee48dc847a139adedf8
Charles <chinaskicharle0@gmail.com>
2024-10-01 11:57:55 -0300

    algunos cambios

--- sivarsmile/src/components/smile.jsx
+++ sivarsmile/src/components/smile.jsx
@@ -1,109 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Card, Button, Row, Col} from 'react-bootstrap';
 
-
-
-/* esta parte es la de arriba y quiero alinear el logo a la izquierda y el boton de mint
-que vaya mas a la derecha. */
-const NavContainer = styled.nav`
-    padding: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-   
-    .logo {
-        margin-right: auto;
-    }
-
-    .nav-links {
-        display: flex;
-        justify-content: center;
-        flex-grow: 1;
-
-        a {
-            margin: 0 1rem;
-            padding: 0.5rem 2rem;
-        }
-    }
-
-    .mint-button {
-        margin-left: 10px;
-        text-transform: uppercase;
-        font-weight: bold;
-        padding: 0.5rem 5rem;
-    }
-`;
 const NavLinks = styled(Nav.Link)`
     font-family: 'Cursive', sans-serif;
     color: #654840;
     font-size: 2.1rem;
-    padding: 0.1rem;
+    // padding: 0.1rem;
     &:hover {
         color: #FF5733;
         background-color: #654840;
         border-radius: 100px;
-        
-    }
-`;
-
-
-/* esta parte es la de abajo */
-
-
-const MainSection = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 2rem;
-`;
-
-const FlexContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 2rem;
-
-    &:nth-child(odd) {
-        flex-direction: row;
-    }
-
-    &:nth-child(even) {
-        flex-direction: row-reverse;
-    }
-`;
-
-const Logo = styled.img`
-    width: 100px;
-    height: auto;
-    margin: 0 1rem;
-`;
-
-const Text = styled.p`
-    font-size: 1.2rem;
-    color: #333;
-    margin: 0 1rem;
-`;
-
-const ButtonMint = styled.button`
-    background-color: #FF5733;
-    border: none;
-    color: white;
-    padding: 1rem 2rem;
-    font-size: 1.5rem;
-    border-radius: 10px;
-    cursor: pointer;
-    &:hover {
-        background-color: #C70039;
     }
 `;
 
 const StyledNavbarCollapse = styled(Navbar.Collapse)`
     @media (max-width: 768px) {
-        transition: max-height 0.5s ease-in-out;
+        transition: max-height 0.3s ease-in-out, opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
         overflow: hidden;
         max-height: ${({ isexpanded }) => (isexpanded ? '500px' : '0')};
+        opacity: ${({ isexpanded }) => (isexpanded ? '1' : '0')};
+        visibility: ${({ isexpanded }) => (isexpanded ? 'visible' : 'hidden')};
     }
 `;
 
@@ -111,47 +29,113 @@ const ResponsiveNavbar = () => {
     const [expanded, setExpanded] = React.useState(false);
 
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="/" className="logo">
-                    <img src="/otrologo2sivar.png" alt="Logo Sivar" width="150" />
+        <Navbar collapseOnSelect bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="/" style={{ marginRight: 'auto' }}>
+                    <img src="/otrologo2sivar.png" alt="Logo Sivar" width="100px" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
                 <StyledNavbarCollapse id="basic-navbar-nav" isexpanded={expanded.toString()}>
-                    <NavContainer>
-                        <div className="nav-links">
-                            <NavLinks href="/">Inicio</NavLinks>
-                            <NavLinks href="/Docs">Docs</NavLinks>
-                            <NavLinks href="/contact">Contact</NavLinks>
-                        </div>
-                        <NavLinks href="/Mint NOW!" className="mint-button">Mint NOW!</NavLinks>
-                    </NavContainer>
+                    <Nav className="me-auto">
+                        <NavLinks href="/">Inicio</NavLinks>
+                        <NavLinks href="/Docs">Docs</NavLinks>
+                        <NavLinks href="/contact">Contact</NavLinks>
+                        
+                    </Nav>
+                        
+                    <Nav>
+                    <NavLinks href="/Mint NOW!" style={{ marginLeft: '10px', textTransform: 'uppercase', fontWeight: 'bold', padding: '0.5rem 2rem', alignContent: 'flex-end' }}>Mint NOW!</NavLinks>
+                    </Nav>
                 </StyledNavbarCollapse>
             </Container>
         </Navbar>
     );
 };
 
+
 const MainContent = () => {
     return (
-        <MainSection>
-            <FlexContainer>
-                <Logo src="/volcanoweb.png" alt="Logo 1" />
-                <Text>Texto al lado del primer logo</Text>
-            </FlexContainer>
-            <FlexContainer>
-                <Logo src="/aboutnft.png" alt="Logo 2" />
-                <Text>Texto al lado del segundo logo</Text>
-            </FlexContainer>
-            <FlexContainer>
-                <Logo src="/ayudaabout.png" alt="Logo 3" />
-                <Text>Texto al lado del tercer logo</Text>
-            </FlexContainer>
-            <div>
-                <Logo src="/letrasivarweb2.png" alt="Mint Logo" />
-                <ButtonMint>MINT NOW!</ButtonMint>
-            </div>
-        </MainSection>
+        <Container style={{ padding: '2rem'}}>
+            <Row className="mb-4">
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Row className="align-items-center">
+                                <Col xs="auto">
+                                    <Card.Img src="/volcanoweb.png" style={{ width: '300px', height: 'auto' }} />
+
+                                </Col>
+                                <Col>
+                                    <Card.Text style={{ fontSize: '1.2rem', color: '#333' }}>
+                                        fortalece a quienes generan cambio a través de donaciones 
+                                        en criptomonedas, creando un impacto positivo donde más se necesita.
+
+                                    </Card.Text>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Row className="align-items-center">
+                            <Col>
+                                    <Card.Text style={{ fontSize: '1.2rem', color: '#333' }}>
+                                    Con la tecnología Web3 y NFTs, recaudamos fondos que van directamente a generar un
+                                     impacto positivo en las personas y comunidades locales.
+                                    </Card.Text>
+                                </Col>
+
+                                <Col xs="auto">
+                                    <Card.Img src="/aboutnft.png" style={{ width: '300px', height: 'auto' }} />
+                                </Col>
+                                
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Row className="align-items-center">
+                                <Col xs="auto">
+                                    <Card.Img src="/ayudaabout.png" style={{ width: '300px', height: 'auto' }} />
+                                </Col>
+                                <Col>
+                                    <Card.Text style={{ fontSize: '1.2rem', color: '#333' }}>
+                                    Nuestro compromiso es garantizar que cada contribución tenga un impacto significativo, 
+                                    brindando esperanza y mejorando la calidad de vida de nuestros beneficiarios. 
+                                    </Card.Text>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <Card>
+                    <Card.Body>
+                            <Row className="align-items-center">
+                                <Col xs="auto">
+                                    <Card.Img src="/letrasivarweb2.png" style={{ width: '500px', height: 'auto' }} />
+                                </Col>
+                                <Col>
+                                    <Button style={{ backgroundColor: '#FF5733', border: 'none', color: 'white', padding: '1rem 2rem', fontSize: '1.5rem', borderRadius: '10px', cursor: 'pointer' }}>
+                                        MINT NOW!
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Card.Body>                  
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
